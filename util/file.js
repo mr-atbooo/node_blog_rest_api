a25f26ef2b657ab5680e4547304d31f0aa1612e1
mr-atbooo <mr.atbooo@gmail.com>
2023-05-01 17:29:38 +0300

    check file is found in directory before delete in node js

--- util/file.js
+++ util/file.js
@@ -1,11 +1,34 @@
 const fs = require('fs');
 
-const deleteFile = (filePath) => {
-    fs.unlink(filePath, (err) => {
+const deleteFile = (filePath) => 
+{
+
+    fs.stat(filePath, function (err, stats) {
+        // console.log(stats);//here we got all information of file in stats variable
+     
         if (err) {
-            throw (err);
+            console.log('file not deleted stat');
+            // return console.error(err);
+        }else{
+
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    // throw (err);
+                    console.log('file not deleted unlink');
+                }else{
+                    console.log('file deleted successfully');
+                }
+            });
+
         }
-    });
+     
+        // fs.unlink('./server/upload/my.csv',function(err){
+        //      if(err) return console.log(err);
+        //      console.log('file deleted successfully');
+        // });  
+     });
+
+   
 }
 
 exports.deleteFile = deleteFile;
